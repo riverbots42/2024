eac289cfc95d623c148d4a12041027101c077361
BJ Black <admin@tacocat.us>
2023-11-15 18:43:43 -0800

    Add comments.

--- doorbell/src/main/java/org/riverbots/door/StatusServlet.java
+++ doorbell/src/main/java/org/riverbots/door/StatusServlet.java
@@ -4,10 +4,15 @@ import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 
+/*
+ * Simple servlet that outputs "ok" and the URL that we received for reference.
+ */
 public class StatusServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int status = HttpServletResponse.SC_OK;
+	// This returns JSON that's basically status:ok and whatever query string we received.
         String ret = String.format("{ \"status\": \"ok\", \"query\": \"%s\" }", request.getRequestURI());
+	// Send the status back to the client.
         response.setContentType("application/json");
         response.setStatus(status);
         response.getWriter().println(ret);
