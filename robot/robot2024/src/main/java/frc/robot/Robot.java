b7f3379fb82d414976d381ae68d935fd6ad8712d
perhapsAmeliorated <amelioratedMail@gmail.com>
2024-01-06 11:55:52 -0800

    Aidan Unfinished

--- robot/robot2024/src/main/java/frc/robot/Robot.java
+++ robot/robot2024/src/main/java/frc/robot/Robot.java
@@ -10,6 +10,8 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
@@ -26,23 +28,21 @@ public class Robot extends TimedRobot {
   Spark leftMotor2 = new Spark(2);
   Spark rightMotor1 = new Spark(3);
   Spark rightMotor2 = new Spark(4);
-
+  MotorControllerGroup m_left = new MotorControllerGroup(leftMotor1, leftMotor2);
+  MotorControllerGroup m_right = new MotorControllerGroup(rightMotor1, rightMotor2);
+  DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
   @Override
   public void robotInit() {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    rightMotor1.setInverted(true);
-    rightMotor2.setInverted(true);
-
-    m_myRobot = new DifferentialDrive(leftMotor1, leftMotor2);
-    m_myRobot = new DifferentialDrive(rightMotor1, rightMotor2);
-    m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
+    
+    
+    m_left.setInverted(true);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_myRobot.tankDrive(-m_leftStick.getY(), -m_rightStick.getY());
+    
   }
 }
