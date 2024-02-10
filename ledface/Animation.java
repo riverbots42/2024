db04c860495ff5d55997a1beb3b8d4540e97c475
BJ Black <admin@tacocat.us>
2024-02-10 14:08:13 -0800

    Add rest of source generator for Animations.java and friends; add test.

--- ledface/Animation.java
+++ ledface/Animation.java
@@ -1,19 +1,34 @@
+package frc.robot;
+
 /**
  * Class describing an array of frames to be displayed.
 **/
 
+import java.util.*;
+
 public class Animation {
     // The array of frame data.
-    public Frame faces[];
+    public Frame frames[];
     // The index of the next frame to be displayed.
-    synchronized public int current;
+    public int current;
 
     /**
      * Initializes an Animation object given a list of Frames.
     **/
     public Animation(Frame src[])
     {
-        faces = src;
+        frames = src;
+        reset();
+    }
+
+    /**
+     * Initializes an Animation object given a Set of Frames.
+    **/
+    public Animation(ArrayList<Frame> src)
+    {
+        Frame dummy[] = new Frame[] {};
+        // Java compiler is dumm; requires a dummy Frame object to coerce type correctly.
+        frames = src.toArray(dummy);
         reset();
     }
 
@@ -29,9 +44,9 @@ public class Animation {
     **/
     public Frame periodic()
     {
-        Frame ret = faces[current];
+        Frame ret = frames[current];
         current++;
-        if (current >= faces.length)
+        if (current >= frames.length)
         {
             reset();
         }
--- ledface/Main.java
+++ ledface/Main.java
@@ -38,40 +38,35 @@ public class Main
 
     /**
      * Given a BufferedImage, convert it to a list of integers (r0, g0, b0, r1, g1, b1, ...)
-     * and then generate a Java source line of the form:
-     *     int identifier[] = { r0, g0, b0, r1, g1, b1, ... }
+     * and then generate a Java source snippet of the form:
+     *     new Frame(new int[] { r0, g0, b0, r1, g1, b1, ... })
      *
-     * @param animation the name of the animation.
      * @param img the BufferedImage to convert to source.
-     * @param identifier the name of the identifier to make.
      * @return a Java source code line.
     **/
-    public static String imageToSource(String animation, BufferedImage img, String identifier)
+    public static String imageToSource(BufferedImage img)
     {
         int width = img.getWidth();
         int height = img.getHeight();
-        // This will eventually hold something like "int identifier[] = { ###, ###, ... };"
-        StringBuilder ret = new StringBuilder();
-        // The first part of the line is "int animation__identifier[] = { "
-        ret.append("int ");
-        ret.append(animation);
-        ret.append("__");
-        ret.append(identifier);
-        ret.append("[] = { ");
+        // This will eventually hold something like "new Frame(new int[] { ###, ###, ... })"
+        StringBuilder ret = new StringBuilder("new Frame(new int[] {");
         // Now we generate the list of ints to put in the file.
-        Color[][] result = new Color[height][width];  // deprecated
         for (int row = 0; row < height; row++) 
         {
             for (int col = 0; col < width; col++) 
             {
-                result[row][col] = new Color(img.getRGB(col, row));
-                ret.append(result[row][col].getRed()); ret.append(", ");
-                ret.append(result[row][col].getGreen()); ret.append(", ");
-                ret.append(result[row][col].getBlue()); ret.append(", ");
+                if (row != 0 || col != 0) {
+                    ret.append(",");
+                }
+                ret.append(" ");
+                Color cur = new Color(img.getRGB(col, row));
+                ret.append(cur.getRed()); ret.append(", ");
+                ret.append(cur.getGreen()); ret.append(", ");
+                ret.append(cur.getBlue());
             }
         }
-        // Finally we close with the "};" to end the line.
-        ret.append("};\n");
+        // Finally we close with the "}"
+        ret.append("})");
         return ret.toString();
     }
 
@@ -82,27 +77,64 @@ public class Main
     public static void main(String args[])
     {
         File files[] = new File(".").listFiles();
+        StringBuilder data = new StringBuilder();
         for(int j=0; j<files.length; j++)
         {
             if(files[j].isDirectory())
             {
-                TreeMap<String,String> rereturn= Iterate(files[j]);
-                Iterator<String> i=rereturn.keySet().iterator();
+                TreeMap<String,String> rereturn = Iterate(files[j]);
+                String id = files[j].getName();
+                if(rereturn.size() < 1)
+                {
+                    continue;
+                }
+                data.append("        ArrayList<Frame> "); data.append(id); data.append(" = new ArrayList<Frame>();\n");
+                Iterator<String> i = rereturn.keySet().iterator();
+                int frameCount = 1;
                 while(i.hasNext())
                 {
                     String key = i.next();
                     try 
                     {
-                        File f = new File (files[j], rereturn.get(key));
-                        BufferedImage winkyFace = ImageIO.read(f);
-                        System.out.println(imageToSource(files[j].getName(), winkyFace, key));
+                        File frameFile = new File(files[j], rereturn.get(key));
+                        BufferedImage frame = ImageIO.read(frameFile);
+                        data.append("        // "); data.append(frameFile.getName());
+                        data.append(" Frame # "); data.append(frameCount); data.append("\n");
+                        data.append("        "); data.append(id); data.append(".add(");
+                        data.append(imageToSource(frame));
+                        data.append(");\n");
+                        if(i.hasNext())
+                        {
+                            data.append(",");
+                        }
+                        data.append("\n");
                     } 
                     catch(IOException E) 
                     {
                         E.printStackTrace();
                     }
                 }
+                data.append("        animations.put(\""); data.append(id); data.append("\", new Animation("); data.append(id); data.append("));\n");
             }
+        }
+        try
+        {
+            BufferedReader template = new BufferedReader(new FileReader("Animations.tmpl"));
+            String line = template.readLine();
+            while(line != null)
+            {
+                if(line.startsWith("// TEMPLATE:"))
+                    ;
+                else if(line.startsWith("// DATA:"))
+                    System.out.println(data.toString());
+                else
+                    System.out.println(line);
+                line = template.readLine();
+            }
+        }
+        catch(IOException E)
+        {        
+            E.printStackTrace();
         }
     }
 }
