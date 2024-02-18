0b6c05103e1305b202fd0c0f442e5e70f784c11a
BJ Black <admin@tacocat.us>
2024-02-17 16:23:51 -0800

    Stupid solution to work around method size limits in Java.

--- ledface/Main.java
+++ ledface/Main.java
@@ -39,17 +39,19 @@ public class Main
     /**
      * Given a BufferedImage, convert it to a list of integers (r0, g0, b0, r1, g1, b1, ...)
      * and then generate a Java source snippet of the form:
-     *     new Frame(new int[] { r0, g0, b0, r1, g1, b1, ... })
+     *     private static int[] id = new int[] { r0, g0, b0, r1, g1, b1, ... })
      *
      * @param img the BufferedImage to convert to source.
      * @return a Java source code line.
     **/
-    public static String imageToSource(BufferedImage img)
+    public static String imageToSource(String id, BufferedImage img)
     {
         int width = img.getWidth();
         int height = img.getHeight();
-        // This will eventually hold something like "new Frame(new int[] { ###, ###, ... })"
-        StringBuilder ret = new StringBuilder("new Frame(new int[] {");
+        // This will eventually hold something like "private static int[] id = new int[] { ###, ###, ... })"
+        StringBuilder ret = new StringBuilder("    private static int[] ");
+        ret.append(id);
+        ret.append("() { return new int[] {");
         // Now we generate the list of ints to put in the file.
         for (int col = 0; col < width; col++) 
         {
@@ -66,7 +68,7 @@ public class Main
             }
         }
         // Finally we close with the "}"
-        ret.append("})");
+        ret.append(" }; }\n");
         return ret.toString();
     }
 
@@ -77,6 +79,7 @@ public class Main
     public static void main(String args[])
     {
         File files[] = new File(".").listFiles();
+        StringBuilder constants = new StringBuilder();
         StringBuilder data = new StringBuilder();
         for(int j=0; j<files.length; j++)
         {
@@ -96,23 +99,22 @@ public class Main
                     String key = i.next();
                     try 
                     {
+                        StringBuffer fid_buf = new StringBuffer(id);
+                        fid_buf.append("__"); fid_buf.append(frameCount);
+                        String fid = fid_buf.toString();
                         File frameFile = new File(files[j], rereturn.get(key));
                         BufferedImage frame = ImageIO.read(frameFile);
                         data.append("        // "); data.append(frameFile.getName());
                         data.append(" Frame # "); data.append(frameCount); data.append("\n");
-                        data.append("        "); data.append(id); data.append(".add(");
-                        data.append(imageToSource(frame));
-                        data.append(");\n");
-                        if(i.hasNext())
-                        {
-                            data.append(",");
-                        }
-                        data.append("\n");
+                        data.append("        "); data.append(id); data.append(".add(new Frame("); data.append(fid); data.append("()));\n");
+                        constants.append(imageToSource(fid, frame));
+                        constants.append("\n");
                     } 
                     catch(IOException E) 
                     {
                         E.printStackTrace();
                     }
+                    frameCount++;
                 }
                 data.append("        animations.put(\""); data.append(id); data.append("\", new Animation("); data.append(id); data.append("));\n");
             }
@@ -125,6 +127,8 @@ public class Main
             {
                 if(line.startsWith("// TEMPLATE:"))
                     ;
+                else if(line.startsWith("// CONSTANTS:"))
+                    System.out.println(constants.toString());
                 else if(line.startsWith("// DATA:"))
                     System.out.println(data.toString());
                 else
